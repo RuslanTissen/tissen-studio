9e1513fc9518d6852fcda70c4a7252dd5137a581
SFERAS <rthyssen73@gmail.com>
2023-04-16 22:17:37 -0300

    introduction adjusted

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -5,7 +5,10 @@ import "./Introduction.scss"
 function Introduction() {
 	return (
 		<div className='introduction'>
-			<h1>Welcome to <mark>Tissen</mark>  studio world.</h1>
+			<div className="introduction__text--container">
+				<h1>Welcome to <mark>Tissen</mark>  studio world.</h1>
+				<p>An optimized website is the biggest lever for your online success today. It is the first and central point of contact for current and potential customers as well as for partners and future employees. The website is therefore decisive for the external image as well as the digital sales and turnover of your brand or your company.</p>
+			</div>
 			<div className="introduction__container">
 				<div className="introduction__image">
 					<img src={intr} alt="" />
