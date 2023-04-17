59e20aeaa8b2a72ba8ef344d7fc83b695b3cf827
SFERAS <rthyssen73@gmail.com>
2023-04-16 22:38:26 -0300

    some text for idea section

--- src/components/idea/idea.jsx
+++ src/components/idea/idea.jsx
@@ -9,7 +9,11 @@ import img5 from "../../images/idea-5.jpg"
 function Idea() {
 	return (
 		<div className='idea'>
-			<div className="idea--container">
+			<div className="idea__text--container">
+				<h1>Design and comfort.</h1>
+				<p>Website design is one of the most valuable tools for user experience. A clear and strict navigation structure plus a harmonious design can greatly enhance the usability of your website.</p>
+			</div>
+			<div className="idea__images--container">
 				<div className="box--img1 box-img">
 					<div className="text__box--h1"><h1>You have an idea!</h1></div>
 					<div className="text__box--p">
