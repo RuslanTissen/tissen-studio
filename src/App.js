2d47ca9424e61c8e3fbc072d27cb42d7847283c6
SFERAS <rthyssen73@gmail.com>
2023-04-19 00:56:49 -0300

    introduction

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
 
 				<Introduction />
 
-				<Templates />
-
 				<Idea />
+
+				<Templates />
 
 				<Plan />
 
--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -5,17 +5,12 @@ import "./Introduction.scss"
 function Introduction() {
 	return (
 		<div className='introduction' id='introduction'>
-			<div className="introduction__text--container">
-				<h1>Welcome to <mark>Tissen</mark>  studio world.</h1>
-				<p>An optimized website is the biggest lever for your online success today. It is the first and central point of contact for current and potential customers as well as for partners and future employees. The website is therefore decisive for the external image as well as the digital sales and turnover of your brand or your company.</p>
+			<div className="introduction__text1">
+				<h1>Tissen  studio</h1>
+				{/* <p>An optimized website is the biggest lever for your online success today. It is the first and central point of contact for current and potential customers as well as for partners and future employees. The website is therefore decisive for the external image as well as the digital sales and turnover of your brand or your company.</p> */}
+				<p>Place where dreams come true</p>
 			</div>
-			<div className="introduction__container">
-				<div className="introduction__image">
-					<img src={intr} alt="" />
-				</div>
-				<div className="introduction__text">
-					<h1>Place where <mark>dreams</mark> come true</h1>
-				</div>
+			<div className="introduction__text2">
 			</div>
 		</div>
 	)
