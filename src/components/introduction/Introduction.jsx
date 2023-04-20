b649867aae7437b9292eeb952d8d50482d2bfe65
SFERAS <rthyssen73@gmail.com>
2023-04-20 20:03:09 -0300

    header adjusted

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -8,7 +8,8 @@ function Introduction() {
 			<div className="introduction__text1">
 				<h1>Tissen web studio</h1>
 				{/* <p>An optimized website is the biggest lever for your online success today. It is the first and central point of contact for current and potential customers as well as for partners and future employees. The website is therefore decisive for the external image as well as the digital sales and turnover of your brand or your company.</p> */}
-				<p>Place where dreams come true</p>
+				{/* <p>Place where dreams come true</p> */}
+				<p>Melody of code</p>
 			</div>
 			<div className="introduction__text2">
 				<Button href='mailto:tissenstudio@gmail.com' variant="contained" color="primary">Contact</Button>
